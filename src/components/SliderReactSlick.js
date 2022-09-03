6890c6c956adcadb86c266587db05bf4bad7f956
locaptech <loc.tk.741@aptechlearning.edu.vn>
2022-09-03 18:45:41 +0700

    Custom slide arrow

--- src/components/SliderReactSlick.js
+++ src/components/SliderReactSlick.js
@@ -1,4 +1,51 @@
 import Slider from 'react-slick';
+import { IconButton } from '@mui/material';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+
+function ArrowNext(props) {
+  const { onClick } = props;
+  return (
+    <IconButton
+      size="small"
+      onClick={onClick}
+      sx={{
+        position: 'absolute',
+        top: '50%',
+        right: {
+          xs: '-25px',
+          md: '-40px',
+          lg: '-50px'
+        },
+        transform: 'translateY(-50%)'
+      }}
+    >
+      <ArrowForwardIosIcon fontSize='12px'/>
+    </IconButton>
+  );
+}
+
+function ArrowPrev(props) {
+  const { onClick } = props;
+  return (
+    <IconButton
+      size="small"
+      onClick={onClick}
+      sx={{
+        position: 'absolute',
+        top: '50%',
+        left:{
+          xs: '-25px',
+          md: '-40px',
+          lg: '-50px'
+        },
+        transform: 'translateY(-50%)'
+      }}
+    >
+      <ArrowBackIosNewIcon fontSize='12px'/>
+    </IconButton>
+  );
+}
 
 const SliderReactSlick = ({ children, isSingle = false }) => {
   const settings = isSingle
@@ -8,8 +55,10 @@ const SliderReactSlick = ({ children, isSingle = false }) => {
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
-        speed: 2000,
+        speed: 1000,
         autoplaySpeed: 7000,
+        prevArrow: <ArrowPrev />,
+        nextArrow: <ArrowNext />,
       }
     : {
         dots: true,
@@ -18,6 +67,8 @@ const SliderReactSlick = ({ children, isSingle = false }) => {
         slidesToShow: 4,
         slidesToScroll: 4,
         initialSlide: 0,
+        prevArrow: <ArrowPrev />,
+        nextArrow: <ArrowNext />,
         responsive: [
           {
             breakpoint: 480,
